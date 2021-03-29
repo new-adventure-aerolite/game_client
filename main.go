5d6bc102de40889f13d1748cfb5609983e0feda0
LiWen <wenli200133@163.com>
2021-03-29 18:57:11 +0800

    update format

--- main.go
+++ main.go
@@ -124,12 +124,26 @@ func VerifyPasscode(passcode string) bool {
 }
 
 func ShowSessionInfo(session, hero map[string]interface{}) {
-	color.Info.Println("  Hero Name: ", hero["name"])
-	color.Info.Println("    Description: ", hero["details"])
-	color.Info.Println("    Live Hero Blood: ", session["live_hero_blood"])
-	color.Info.Println("    Live Boss Blood: ", session["live_boss_blood"])
-	color.Info.Println("    Current Level: ", session["current_level"])
-	color.Info.Println("    Score: ", session["score"])
+	color.Info.Printf("%30s %s\n", "  Hero Name: ", hero["name"])
+	color.Info.Printf("%30s %s\n", "    Description: ", hero["details"])
+	liveheroblood := session["live_hero_blood"].(float64)
+	livebossblood := session["live_boss_blood"].(float64)
+	currentlevel := session["current_level"].(float64)
+	// score := session["score"].(float64)
+	var liveheroblood1 int = int(liveheroblood)
+	var livebossblood1 int = int(livebossblood)
+	var currentlevel1 int = int(currentlevel)
+	color.Info.Printf("%30s %d\n", "    Live Hero Blood: ", liveheroblood1)
+	color.Info.Printf("%30s %d\n", "    Live Boss Blood: ", livebossblood1)
+	color.Info.Printf("%30s %d\n", "    Current Level: ", currentlevel1)
+	var score float64 = 0
+	if session["score"] == nil {
+		score = 0
+	} else {
+		score = session["score"].(float64)
+	}
+	var score1 int = int(score)
+	color.Info.Printf("%30s %d\n", "    Score: ", score1)
 }
 
 func main() {
@@ -137,7 +151,7 @@ func main() {
 	var err error
 	var statusCode int
 	reader := bufio.NewReader(os.Stdin)
-
+	var heroName string = ""
 	token := GetToken()
 	if token == "" {
 		color.Green.Printf("Please Open %s in your browser\n", auth.AuthLoginUrl)
@@ -174,7 +188,7 @@ func main() {
 	method := interact.SelectOne(
 		"You want to play game from beginning or continue from last time?",
 		map[string]string{"1": "From beginning", "2": "Continue"},
-		"2",
+		"1",
 		false,
 	)
 	color.Info.Println("Your select is:", method)
@@ -218,35 +232,35 @@ func main() {
 		}
 		// color.Info.Println("Description: ", setHero.Hero["details"])
 		ShowSessionInfo(setHero.Session, setHero.Hero)
+		heroName = setHero.Hero["name"].(string)
 	} else {
-		// color.Info.Println("The hero you choose is:", sessionView.Hero["name"])
-		// color.Info.Println("Description: ", sessionView.Hero["details"])
 		ShowSessionInfo(sessionView.Session, sessionView.Hero)
+		heroName = sessionView.Hero["name"].(string)
 	}
 	fmt.Println("----------------------------------------------------------")
 	for {
 		action := interact.SingleSelect(
 			"Your action?",
-			map[string]string{"1": "Fight", "2": "Archive", "3": "Quit"},
+			map[string]string{"1": "Fight", "2": "Archive", "3": "Reset", "4": "Quit"},
 			"1",
 			false,
 		)
-		// call fight api
+
 		switch action {
 		case "Fight":
-			// color.Info.Println("Your select is:", action)
+			fmt.Println("----------------------------------------------------------")
+			color.Info.Printf("  %s %s \n", heroName, action)
 			fightResp, err := auth.Fight(token)
 			if err != nil {
 				color.Error.Println("An error occured while fight !!!!", err)
 			}
-
-			color.Info.Println("      Hero Blood  ", fightResp.HeroBlood)
-			color.Info.Println("      Boss Blood  ", fightResp.BossBlood)
-			color.Info.Println("      Next Level  ", fightResp.NextLevel)
-			color.Info.Println("      Score  ", fightResp.Score)
+			color.Info.Printf("%30s %d\n", "      Hero Blood  ", fightResp.HeroBlood)
+			color.Info.Printf("%30s %d\n", "      Boss Blood  ", fightResp.BossBlood)
+			color.Info.Printf("%30s %v\n", "      Next Level  ", fightResp.NextLevel)
+			color.Info.Printf("%30s %d\n", "      Score  ", fightResp.Score)
 
 			if fightResp.GameOver || fightResp.HeroBlood == 0 {
-				color.Info.Println("Game Over")
+				color.Info.Println("  Game Over")
 				msg, err := auth.QuitSession(token)
 				if err != nil {
 					color.Error.Println("An error occured while quit session !!!!", err)
@@ -261,7 +275,7 @@ func main() {
 				}
 				if session1.Session["current_level"].(float64) >= 2 {
 					// game over
-					color.Info.Println("Congratulations, You Win the Game, ByeBye")
+					color.Info.Println("  Congratulations, You Win the Game, ByeBye")
 					err := auth.ClearSession(token)
 					if err != nil {
 						color.Error.Println("An error occured while clear session !!!!", err)
@@ -273,32 +287,27 @@ func main() {
 					color.Error.Println("An error occured while goes into next level !!!!", err)
 				}
 				if nextLevelResp != nil && nextLevelResp.Passed {
-					color.Info.Println("Congratulations, You Win the Game, ByeBye")
+					color.Info.Println("  Congratulations, You Win the Game, ByeBye")
 					err := auth.ClearSession(token)
 					if err != nil {
 						color.Error.Println("An error occured while clear session !!!!", err)
 					}
 					return
 				}
-				color.Info.Println("You have gone into next level !!!")
-				// if nextLevelResp != nil && nextLevelResp.Session != nil {
-				// 	curLevel := nextLevelResp.Session["current_level"]
-				// 	if curLevel != nil {
-				// 		if curLevel.(float64) > 2 {
-				// 			return
-				// 		}
-				// 	}
-				// }
+				color.Info.Println("  You have gone into next level !!!")
 			}
 		case "Archive":
-			color.Info.Println("Your select is:", action)
 			sessionid, err := auth.ArchiveSession(token)
 			if err != nil {
 				color.Error.Println("An error occured while archive session !!!!", err)
 			}
 			color.Info.Println(fmt.Sprintf("SessionID %s is archived", sessionid))
+		case "Reset":
+			err := auth.ClearSession(token)
+			if err != nil {
+				color.Error.Println("An error occured while clear session !!!!", err)
+			}
 		case "Quit":
-			color.Info.Println("Your select is:", action)
 			msg, err := auth.QuitSession(token)
 			if err != nil {
 				color.Error.Println("An error occured while quit session !!!!", err)
@@ -306,8 +315,7 @@ func main() {
 			color.Info.Println(msg)
 			return
 		default:
-			color.Info.Println("Your select is:", action)
-			color.Info.Println("Calling Fight API")
+			color.Info.Println(action)
 		}
 	}
 }
