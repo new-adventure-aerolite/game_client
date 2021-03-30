6e132e3f3528e4675887e3b94b944a875be3afe0
LiWen <wenli200133@163.com>
2021-03-30 14:28:32 +0800

    uadd cobra

--- go.mod
+++ go.mod
@@ -3,7 +3,11 @@ module github.com/new-adventure-aerolite/game-client
 go 1.16
 
 require (
+	github.com/gookit/color v1.3.8
+	github.com/gookit/gcli/v3 v3.0.0-20210329160531-2f2b598a497c
 	github.com/mitchellh/go-homedir v1.1.0
+	github.com/pkg/browser v0.0.0-20210115035449-ce105d075bb4
 	github.com/spf13/cobra v1.1.3
 	github.com/spf13/viper v1.7.1
+	gopkg.in/ini.v1 v1.51.0
 )
--- pkg/client/client.go
+++ pkg/client/client.go
@@ -4,111 +4,245 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
+	"regexp"
 	"strings"
 
-	"github.com/new-adventure-aerolite/game-client/pkg/types"
+	"github.com/gookit/color"
+	"github.com/gookit/gcli/v3/interact"
+	"github.com/new-adventure-aerolite/game-client/pkg/auth"
+	"github.com/pkg/browser"
+	"gopkg.in/ini.v1"
 )
 
+const ConfigFile = ".game/config"
+
+func GetToken() string {
+	homePath := os.Getenv("HOME")
+	configFilePath := fmt.Sprintf("%s/%s", homePath, ConfigFile)
+	cfg, err := ini.Load(configFilePath)
+	if err != nil {
+		color.Error.Printf("Fail to read config file: %v", err)
+		os.Exit(1)
+	}
+	return cfg.Section("").Key("id_token").String()
+}
+
+func SetToken(token string) {
+	homePath := os.Getenv("HOME")
+	configFilePath := fmt.Sprintf("%s/%s", homePath, ConfigFile)
+	cfg, err := ini.Load(configFilePath)
+	if err != nil {
+		color.Error.Printf("Fail to read config file: %v", err)
+		os.Exit(1)
+	}
+
+	cfg.Section("").Key("id_token").SetValue(token)
+	cfg.SaveTo(configFilePath)
+
+}
+
+func VerifyEmailFormat(email string) bool {
+	pattern := `^[0-9a-z][_.0-9a-z-]{0,31}@([0-9a-z][0-9a-z-]{0,30}[0-9a-z]\.){1,4}[a-z]{2,4}$`
+	reg := regexp.MustCompile(pattern)
+	return reg.MatchString(email)
+}
+
+func VerifyPasscode(passcode string) bool {
+	pattern := `\d{6}`
+	reg := regexp.MustCompile(pattern)
+	return reg.MatchString(passcode)
+}
+
+func ShowSessionInfo(session, hero map[string]interface{}) {
+	color.Info.Printf("%30s %s\n", "  Hero Name: ", hero["name"])
+	color.Info.Printf("%30s %s\n", "    Description: ", hero["details"])
+	liveheroblood := session["live_hero_blood"].(float64)
+	livebossblood := session["live_boss_blood"].(float64)
+	currentlevel := session["current_level"].(float64)
+	// score := session["score"].(float64)
+	var liveheroblood1 int = int(liveheroblood)
+	var livebossblood1 int = int(livebossblood)
+	var currentlevel1 int = int(currentlevel)
+	color.Info.Printf("%30s %d\n", "    Live Hero Blood: ", liveheroblood1)
+	color.Info.Printf("%30s %d\n", "    Live Boss Blood: ", livebossblood1)
+	color.Info.Printf("%30s %d\n", "    Current Level: ", currentlevel1)
+	var score float64 = 0
+	if session["score"] == nil {
+		score = 0
+	} else {
+		score = session["score"].(float64)
+	}
+	var score1 int = int(score)
+	color.Info.Printf("%30s %d\n", "    Score: ", score1)
+}
+
 func Start() {
-	var emailAddress = ""
 	var err error
+	var statusCode int
+	var heroName string = ""
 	reader := bufio.NewReader(os.Stdin)
-	for {
-		fmt.Print("Please Input your email address:")
-		// ReadString will block until the delimiter is entered
-		emailAddress, err = reader.ReadString('\n')
-		if err != nil {
-			fmt.Println("An error occured while reading input. Please try again", err)
-			return
-		}
-		// remove the delimeter from the string
-		emailAddress = strings.TrimSuffix(emailAddress, "\n")
-		if emailAddress != "" && len(emailAddress) != 0 {
-			fmt.Println(emailAddress)
-			break
-		} else {
-			fmt.Println("Your email address is empty, Please try again", err)
-		}
-	}
-	// calling load session api
-	loadSession := &types.Session{
-		UID:           "1",
-		HeroName:      "",
-		LiveHeroBlood: 3,
-		LiveBossBlood: 4,
-		CurrentLevel:  5,
-		Score:         6,
-	}
-
-	if loadSession.HeroName == "" && len(loadSession.HeroName) == 0 {
-		fmt.Println("HeroName is empty")
-		// calling  get hero list api
-		heroList := &types.HeroList{
-			Heros: []types.Hero{
-				{
-					Name:         "hero1",
-					Details:      "1",
-					AttackPower:  2,
-					DefensePower: 3,
-					Blood:        4,
-				},
-				{
-					Name:         "hero2",
-					Details:      "2",
-					AttackPower:  2,
-					DefensePower: 3,
-					Blood:        4,
-				},
-			},
-		}
-		fmt.Println("You can choose one Hero from below list:")
-		for index, hero := range heroList.Heros {
-			fmt.Printf("%d: %s\n", index, hero.Name)
-		}
-		var heroNum int
-		fmt.Scanf("%d", &heroNum)
-		choosenHero := heroList.Heros[heroNum].Name
-		// call get hero api
-		fmt.Println("You have choosen Hero:", choosenHero)
-
-	}
-	for {
-		fmt.Print("Please choose a game action:\n 1: Fight \n 2: Archive\n 3: Quit\n")
-		action, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Println("An error occured while reading input. Please try again", err)
-			return
-		}
-		actionNum1 := strings.TrimSuffix(action, "\n")
-		actionNum, err := strconv.ParseInt(actionNum1, 10, 32)
-		fmt.Println(actionNum)
-		// call fight api
-		switch actionNum {
-		case 1:
-			fmt.Println("Calling Fight API: ")
-			fight := &types.Fight{
-				GameOver:  false,
-				NextLevel: true,
-				Score:     0,
-				HeroBlood: 20,
-				BossBlood: 80,
+Reset:
+	token := GetToken()
+	if token == "" {
+		color.Green.Printf("Please Open %s in your browser\n", auth.AuthLoginUrl)
+		browser.OpenURL(auth.AuthLoginUrl)
+		color.Green.Printf("Then copy passcode from your browser to here: ")
+		var passcode string
+		for {
+			passcode, err = reader.ReadString('\n')
+			if err != nil {
+				color.Error.Println("An error occured while get passcode. Please try again", err)
+				return
 			}
-			if fight.BossBlood == 0 {
-				fmt.Println("Enter next level:")
-			} else {
-				if fight.HeroBlood == 0 {
-					break
+			// remove the delimeter from the string
+			passcode = strings.TrimSuffix(passcode, "\n")
+			if passcode != "" && len(passcode) == 6 {
+				if !VerifyPasscode(passcode) {
+					color.Warn.Println("Your passcode is incorrect, Please try again", err)
 				}
 			}
-		case 2:
-			fmt.Println("Calling Archive API:")
-		case 3:
-			fmt.Println("Quit:")
-			return
-		default:
-			fmt.Println("Calling Fight API:")
+			token, statusCode, err = auth.RequestToken(passcode)
+			if err != nil {
+				color.Error.Println("An error occured while request token !!!!", err)
+				return
+			}
+			if statusCode == 401 {
+				color.Error.Println("Unauthorized occured while request token !!!!", statusCode)
+			} else {
+				break
+			}
 		}
+		SetToken(token)
+		color.Green.Println("Login Successfully")
 	}
 
+	method := interact.SelectOne(
+		"You want to play game from beginning or continue from last time?",
+		map[string]string{"1": "From beginning", "2": "Continue"},
+		"1",
+		false,
+	)
+	color.Info.Println("Your select is:", method)
+	if method != "Continue" {
+		err = auth.ClearSession(token)
+		if err != nil {
+			color.Error.Println("An error occured while clear session !!!!", err)
+		}
+	}
+	sessionView, err := auth.LoadSession(token)
+	if err != nil {
+		color.Error.Println("An error occured while load session !!!!", err)
+	}
+
+	if len(sessionView.Hero) == 0 || sessionView.Hero["name"] == nil {
+		// calling  get hero list api
+		heros, err := auth.RequestHeros(token)
+		if err != nil {
+			color.Error.Println("An error occured while load heros!!!!", err)
+		}
+		fmt.Println("----------------------------------------------------------")
+		color.Info.Println("You can choose one Hero from below list:")
+
+		var heroNameList = make([]string, len(heros))
+		for index, hero := range heros {
+			heroNameList[index] = hero.Name
+		}
+
+		choosenHero := interact.Choice(
+			"Choose Hero?",
+			heroNameList,
+			"",
+			false,
+		)
+		// call get hero api
+		color.Info.Println("Your select is:", choosenHero)
+		setHero, err := auth.SetHero(choosenHero, token)
+		if err != nil {
+			color.Error.Println("An error occured while set hero !!!!", err)
+		}
+		// color.Info.Println("Description: ", setHero.Hero["details"])
+		ShowSessionInfo(setHero.Session, setHero.Hero)
+		heroName = setHero.Hero["name"].(string)
+	} else {
+		ShowSessionInfo(sessionView.Session, sessionView.Hero)
+		heroName = sessionView.Hero["name"].(string)
+	}
+	fmt.Println("----------------------------------------------------------")
+	for {
+		action := interact.SingleSelect(
+			"Your action?",
+			map[string]string{"1": "Fight", "2": "Archive", "3": "Reset", "4": "Quit"},
+			"1",
+			false,
+		)
+
+		switch action {
+		case "Fight":
+			fmt.Println("----------------------------------------------------------")
+			color.Info.Printf("  %s %s \n", heroName, action)
+			fightResp, err := auth.Fight(token)
+			if err != nil {
+				color.Error.Println("An error occured while fight !!!!", err)
+			}
+			color.FgLightBlue.Printf("%30s %d\n", "      Hero Blood  ", fightResp.HeroBlood)
+			color.FgLightMagenta.Printf("%30s %d\n", "      Boss Blood  ", fightResp.BossBlood)
+			color.FgLightBlue.Printf("%30s %v\n", "      Next Level  ", fightResp.NextLevel)
+			color.FgLightBlue.Printf("%30s %d\n", "      Score  ", fightResp.Score)
+
+			if fightResp.GameOver || fightResp.HeroBlood == 0 {
+				color.Info.Println("  Game Over")
+				msg, err := auth.QuitSession(token)
+				if err != nil {
+					color.Error.Println("An error occured while quit session !!!!", err)
+				}
+				color.Info.Println(msg)
+				return
+			}
+			if fightResp.NextLevel || fightResp.BossBlood == 0 {
+				session1, err := auth.LoadSession(token)
+				if err != nil {
+					color.Error.Println("An error occured while load seesion !!!!", err)
+				}
+				if session1.Session["current_level"].(float64) >= 2 {
+					// game over
+					color.Info.Println("  Congratulations, You Win the Game, ByeBye")
+					err := auth.ClearSession(token)
+					if err != nil {
+						color.Error.Println("An error occured while clear session !!!!", err)
+					}
+					return
+				}
+				nextLevelResp, err := auth.NextLevel(token)
+				if err != nil {
+					color.Error.Println("An error occured while goes into next level !!!!", err)
+				}
+				if nextLevelResp != nil && nextLevelResp.Passed {
+					color.Info.Println("  Congratulations, You Win the Game, ByeBye")
+					err := auth.ClearSession(token)
+					if err != nil {
+						color.Error.Println("An error occured while clear session !!!!", err)
+					}
+					return
+				}
+				color.Info.Println("  You have gone into next level !!!")
+			}
+		case "Archive":
+			sessionid, err := auth.ArchiveSession(token)
+			if err != nil {
+				color.Error.Println("An error occured while archive session !!!!", err)
+			}
+			color.Info.Println(fmt.Sprintf("SessionID %s is archived", sessionid))
+		case "Reset":
+			goto Reset
+		case "Quit":
+			msg, err := auth.QuitSession(token)
+			if err != nil {
+				color.Error.Println("An error occured while quit session !!!!", err)
+			}
+			color.Info.Println(msg)
+			return
+		default:
+			color.Info.Println(action)
+		}
+	}
 }
